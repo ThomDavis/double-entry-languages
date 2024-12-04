6075be332edb91dcd109e296753925147c1aafc9
Thom Davis <Techthomdavis@gmail.com>
2024-12-04 11:44:31 -0600

    composer init

---
+++ php/php84/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'thomdavis/php84',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '821db5c214f0cbdb15fcb7ab463dee9d3461319e',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'thomdavis/php84' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '821db5c214f0cbdb15fcb7ab463dee9d3461319e',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
